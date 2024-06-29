2bdd6cff954c9504e562988c3142862499c521bf
Nhi4912 <thaonhi8a8@gmail.com>
2024-06-30 00:31:01 +0700

    Test line

--- addBorder/addBorder.ts
+++ addBorder/addBorder.ts
@@ -1,6 +1,5 @@
 export const addBorder = (picture: string[]): string[] => {
   const pictureLength = picture[0].length;
-
   const border = "*".repeat(pictureLength + 2);
   const contentWithHorizontalBorder = picture.map((item) => `*${item}*`);
 
