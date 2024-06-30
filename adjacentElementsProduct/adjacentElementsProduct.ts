b91d391342cd020bcc347a2b482936a06014796d
Nhi4912 <thaonhi8a8@gmail.com>
2024-07-01 00:22:11 +0700

    Resolve adjacentElementsProduct

--- adjacentElementsProduct/adjacentElementsProduct.ts
+++ adjacentElementsProduct/adjacentElementsProduct.ts
@@ -1,5 +1,16 @@
-export function adjacentElementsProduct(inputArray: number[]): number {
+export const adjacentElementsProduct = (inputArray: number[]): number => {
+  let max = 0;
 
-}
+  inputArray.forEach((item, index) => {
+    if (index < inputArray.length - 1) {
+      const currentResult = item * inputArray[index + 1];
+      if (currentResult >= max) {
+        max = currentResult;
+      }
+    }
+  });
+
+  return max;
+};
 
 //console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]));
